445a2f500423eca218564150383b836f1cf28b00
SnO2WMaN <15155608+SnO2WMaN@users.noreply.github.com>
2021-01-08 20:35:06 +0900

    feat: ApolloClientの設定

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  env: {
+    GRAPHQL_API_ENDPOINT: process.env.GRAPHQL_API_ENDPOINT,
+  },
   images: {
     domains: ['cover.openbd.jp'],
   },
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable no-process-env */
 import clsx from 'clsx';
 import {AppProps} from 'next/app';
 import React from 'react';
 import 'tailwindcss/tailwind.css';
 import {ConfiguratedI18nextProvider} from '~/i18n';
+import {ConfiguredApolloProvider} from '~/lib/apollo-provider';
 import '~/styles/index.css';
 
 export const App: React.FC<AppProps> = ({
@@ -10,11 +12,13 @@ export const App: React.FC<AppProps> = ({
   pageProps,
 }) => {
   return (
-    <ConfiguratedI18nextProvider>
-      <div className={clsx('container', 'mx-auto', 'pt-12')}>
-        <PageComponent {...pageProps} />
-      </div>
-    </ConfiguratedI18nextProvider>
+    <ConfiguredApolloProvider apiEndpoint={process.env.GRAPHQL_API_ENDPOINT!}>
+      <ConfiguratedI18nextProvider>
+        <div className={clsx('container', 'mx-auto', 'pt-12')}>
+          <PageComponent {...pageProps} />
+        </div>
+      </ConfiguratedI18nextProvider>
+    </ConfiguredApolloProvider>
   );
 };
 
