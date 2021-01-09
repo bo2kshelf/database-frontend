c625c91a2e775c4ecb76e8f3ee68dea3a890b4d2
SnO2WMaN <15155608+SnO2WMaN@users.noreply.github.com>
2021-01-10 00:03:18 +0900

    fix: Containerのexport名修正

--- src/components/SearchBox/parts/Suggestion/index.ts
+++ src/components/SearchBox/parts/Suggestion/index.ts
@@ -2,5 +2,5 @@ export {BaseComponent as ComponentBase} from './BaseComponent';
 export type {BaseComponentProps as ComponentBaseProps} from './BaseComponent';
 export {Component} from './Component';
 export type {ComponentProps} from './Component';
-export {Container} from './Container';
-export type {ContainerProps} from './Container';
+export {Container as Sugesstion} from './Container';
+export type {ContainerProps as SugesstionProps} from './Container';
